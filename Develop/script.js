3a618bb03680a1e478999fc288052650f7d37755
Mike Bannoura <mikebannoura@gmail.com>
2020-10-31 15:15:08 -0600

    got api to append all info in cards.

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,135 @@
-var city = "denver"
-var apiWeather = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+',usa&appid=cda51abe369881d5a2a4986a8b0a39d0';
+var userInput =""
+var userInput = localStorage.getItem("Search-city", userInput); 
+
+var key = "cda51abe369881d5a2a4986a8b0a39d0";
+var apiWeather = 'https://api.openweathermap.org/data/2.5/forecast?q='+userInput+',usa&units=imperial&appid='+key;
+
+
+
 fetch(apiWeather)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // console.log(data);
-      
-      for (var i = 0; i < data.length; i++) {
-      console.log(data[i]);
-      
-    }
-    });
+      console.log(data)
 
-// function getApi() {
+      $("#cityName").text(moment().format("dddd, MMMM Do"));
+      $("#cityName").append(" "+data.city.name); 
+      var getIcon = data.list[0].weather[0].icon;
+      var icon = "http://openweathermap.org/img/wn/"+getIcon+"@2x.png";
+      $("#cityName").append('<img src=' + icon + '>');
+      $("#cityName").append("<br/><br/>" + "Temperature: "+data.list[0].main.temp +" F");
+      $("#cityName").append("<br/><br/>" + "Humidity: "+data.list[0].main.humidity +"%");
+      $("#cityName").append("<br/><br/>" + "Wind Speed: "+data.list[0].wind.speed +" MPH");
+
+      var laT = data.city.coord.lat;
+      var loN = data.city.coord.lon;
+      var uV = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat="+laT+"&lon="+loN+"&cnt=1&appid="+key;
+      
+    fetch(uV)
+    .then(function (uVresponse) {
+      return uVresponse.json();
+    })
+    .then(function (data) {
+      console.log(data[0].value);
+      $("#cityName").append("<br/><br/>" + "UV Index: "+data[0].value);
+    });
+ forcastWeather(laT, loN);
+   
+});
+
+function forcastWeather (laT, loN) {
+var daily = 'https://api.openweathermap.org/data/2.5/onecall?lat='+laT+'&lon='+loN+'&units=imperial&appid='+key;
+ fetch(daily)
+    .then(function (dResponse) {
+      return dResponse.json();
+    })
+    .then(function (dData) {
+      var cardOne = $("#day-one");
+      var cardTwo = $("#day-two");
+      var cardThree = $("#day-three");
+      var cardFour = $("#day-four");
+      var cardFive = $("#day-five");
+
+
+  function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var time = month + '/' + date + '/' + year;
+  return time;
+}
+    if (dData.daily.length > 0) {
+      var getDailyIconOne = dData.daily[0].weather[0].icon;
+      var iconOne = "http://openweathermap.org/img/wn/"+getDailyIconOne+"@2x.png";
+      var timeOne = $("<p>");
+      var tempOne = $("<p>");
+      timeOne.text(timeConverter(dData.daily[1].dt));
+      cardOne.append(timeOne);
+      tempOne.text("Temp: " + dData.daily[0].temp.day + " F")
+      cardOne.append('<img src=' + iconOne + '>');
+      cardOne.append(tempOne);
+      
+      var getDailyIconTwo = dData.daily[1].weather[0].icon;
+      var iconTwo = "http://openweathermap.org/img/wn/"+getDailyIconTwo+"@2x.png";
+      var timeTwo = $("<p>");
+      var tempTwo = $("<p>");
+      timeTwo.text(timeConverter(dData.daily[1].dt));
+      cardTwo.append(timeTwo);
+      tempTwo.text("Temp: " + dData.daily[1].temp.day + " F");
+      cardTwo.append('<img src=' + iconTwo + '>');
+      cardTwo.append(tempTwo);
+
+      var getDailyIconThree = dData.daily[2].weather[0].icon;
+      var iconThree = "http://openweathermap.org/img/wn/"+getDailyIconThree+"@2x.png";
+      var timeThree = $("<p>");
+      var tempThree = $("<p>");
+      timeThree.text(timeConverter(dData.daily[3].dt));
+      cardThree.append(timeThree);
+      tempThree.text("Temp: " + dData.daily[2].temp.day + " F")
+      cardThree.append('<img src=' + iconThree + '>');
+      cardThree.append(tempThree);
+
+      var getDailyIconFour = dData.daily[3].weather[0].icon;
+      var iconFour = "http://openweathermap.org/img/wn/"+getDailyIconFour+"@2x.png";
+      var timeFour = $("<p>");
+      var tempFour = $("<p>");
+      timeFour.text(timeConverter(dData.daily[4].dt));
+      cardFour.append(timeFour);
+      tempFour.text("Temp: " + dData.daily[3].temp.day + " F")
+      cardFour.append('<img src=' + iconFour+ '>');
+      cardFour.append(tempFour);
+
+      var getDailyIconFive = dData.daily[4].weather[0].icon;
+      var iconFive = "http://openweathermap.org/img/wn/"+getDailyIconFive+"@2x.png";
+      var timeFive = $("<p>");
+      var tempFive = $("<p>");
+      timeFive.text(timeConverter(dData.daily[5].dt));
+      cardFive.append(timeFive);
+      tempFive.text("Temp: " + dData.daily[4].temp.day + " F")
+      cardFive.append('<img src=' + iconFive + '>');
+      cardFive.append(tempFive);
+      }
+       console.log(dData.daily);    
+    }
+    );
+
+
+
+};
+
+$(".btn").on("click", function (event) {
+  event.preventDefault()
+  userInput = $(".form-control").val();
+  localStorage.setItem("Search-city", userInput);
+
   
-// };
-// getApi();
+  list = $("<li>").text(userInput);
+  $("#city-list").append(list);
+  localStorage.setItem("City-list", list);
+  
+   
+
+});
